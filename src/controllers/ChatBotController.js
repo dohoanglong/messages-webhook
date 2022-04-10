4692a63c4cb55da56f754093ef051dd7d52768cd
dohoanglong <49149829+dohoanglong@users.noreply.github.com>
2022-04-10 12:13:00 +0700

    handle message

--- src/controllers/ChatBotController.js
+++ src/controllers/ChatBotController.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import request from 'request';
 
 const PAGE_ACCESS_TOKEN = process.env.MY_VERIFY_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
@@ -23,6 +24,14 @@ const receiveEvent = (req, res) => {
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         // Returns a '200 OK' response to all requests
@@ -60,7 +69,19 @@ const verifyWebhook = (req, res) => {
 }
 
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Check if the message contains text
+    if (received_message.text) {    
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }  
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response); 
 }
 
 // Handles messaging_postbacks events
@@ -70,7 +91,26 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+    let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+      }
+    
+      // Send the HTTP request to the Messenger Platform
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
 }
 
 export default {
